3f2a45f3d93269156fc3bd4d8db0407cd9cda31c
KPG <hybridremix@yahoo.com>
2022-10-24 00:48:13 -0400

    Initial commit

---
+++ analyzer.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+import wordcloud
+
+df = pd.read_csv("white-house-data.csv", index_col=0)
+df.groupby("issue").title.count().plot(
+    kind="pie",
+    title="Trump White House News Release by Issue",
+    ylabel="",
+    autopct="%.1f%%"
+)
+plt.show()
+
+df.groupby("type").title.count().plot(
+    kind="pie",
+    title="Trump White House News Release by Type",
+    ylabel="",
+    autopct="%.1f%%"
+)
+plt.show()
+
+df["date"] = pd.to_datetime(df["date"])
+df["year"] = df["date"].apply(lambda d: datetime(year=d.year, month=1, day=1))
+df["month"] = df["date"].apply(lambda d: datetime(year=d.year, month=d.month, day=1))
+
+df_year_issue = pd.DataFrame(df.groupby(["year", "issue"]).title.count())
+df_year_issue.pivot_table(values="title", index="year", columns="issue").plot(
+    kind="area",
+    title="Trump White House News Relase by Issue and Year"
+)
+plt.show()
+
+df_year_type = pd.DataFrame(df.groupby(["year", "type"]).title.count())
+df_year_type.pivot_table(values="title", index="year", columns="type").plot(
+    kind="area",
+    title="Trump White House News Relase by Type and Year"
+)
+plt.show()
+
+df_month_issue = pd.DataFrame(df.groupby(["month", "issue"]).title.count())
+df_month_issue.pivot_table(values="title", index="month", columns="issue").plot(
+    kind="area",
+    title="Trump White House News Relase by Issue and Month"
+)
+plt.show()
+
+df_month_type = pd.DataFrame(df.groupby(["month", "type"]).title.count())
+df_month_type.pivot_table(values="title", index="month", columns="type").plot(
+    kind="area",
+    title="Trump White House News Relase by Type and Month"
+)
+plt.show()
+
+text = " ".join(df["title"].to_list())
+cloud = wordcloud.WordCloud(
+    width=1920,
+    height=1080,
+    background_color="white"
+)
+
+cloud.generate(text)
+plt.imshow(cloud)
+plt.axis("off")
+plt.show()
---
+++ scraper.py
@@ -0,0 +1,45 @@
+import time
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+data_table = {
+    "title": [],
+    "type": [],
+    "issue": [],
+    "date": [],
+}
+
+page_num = 800
+
+while True:
+    print(f"Scraping page {page_num} ...")
+    response = requests.get(f"https://trumpwhitehouse.archives.gov/news/page/{page_num}")
+    
+    if response.status_code != 200:
+        print("Ending...")
+        break
+    
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    for article in soup.find_all("article"):
+        article_class = article["class"][0]
+        type_ = article.find("p", class_=f"{article_class}__type")
+        issue_ = article.find("p", class_="issue-flag")
+
+        data_table["title"].append(article.find("h2", class_=f"{article_class}__title").text.strip())
+        data_table["type"].append(type_.text.strip() if type_ is not None else "Other")
+        data_table["issue"].append(issue_.text.strip() if issue_ is not None else "Other")
+        data_table["date"].append(article.find("time").text.strip())
+
+        time.sleep(0.5)
+
+    page_num += 1
+
+print(len(data_table["title"]))
+print(len(data_table["type"]))
+print(len(data_table["issue"]))
+print(len(data_table["date"]))
+
+df = pd.DataFrame(data_table)
+df.to_csv("white-house-data.csv")
